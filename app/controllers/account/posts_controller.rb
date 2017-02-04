4126832c0106e0d114912de5179634bdce4426bd
yaoyizhi <email@yaoadou888>
2017-02-04 17:25:06 +0800

    fail to implement edit&delete to post

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,14 @@ class Account::PostsController < ApplicationController
     def index
         @posts = current_user.posts
     end
+
+    def edit
+        @post = current_user.posts.find(param[:id])
+    end
+
+    def destroy
+        @post.destroy
+        flash[:alert] = 'Group deleted'
+        redirect_to groups_path
+    end
 end
